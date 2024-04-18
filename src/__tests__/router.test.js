e69456bd3bc03f24390afa1c434077f34ab13eae
yoonvelop <yoonvelop@gmail.com>
2024-04-18 16:09:05 +0900

    feat: 라우터 테스트 코드안에 mock 페이지 Class 만들어서 테스트

--- src/__tests__/router.test.js
+++ src/__tests__/router.test.js
@@ -1,18 +1,45 @@
 import { Router } from '../router';
-import PostsPage from '../pages/postsPage';
-import PostPage from '../pages/postPage';
-import NotFound from '../pages/notFoundPage';
-
 describe('Router', () => {
   let router;
+
+  // 페이지 컴포넌트 클래스 정의
+  class PostsPageMock {
+    async render() {
+      const div = document.createElement('div');
+      div.innerText = '리스트';
+      return div;
+    }
+  }
+
+  class PostPageMock {
+    constructor({ searchParams }) {
+      this.searchParams = searchParams;
+    }
+    async render() {
+      const div = document.createElement('div');
+      const id = this.searchParams.get('id');
+      div.innerText = id ? `디테일: ${id}` : '해당 id를 가진 게시글이 없음';
+      return div;
+    }
+  }
+
+  class NotFoundPageMock {
+    async render() {
+      const div = document.createElement('div');
+      div.innerText = 'NOT FOUND';
+      return div;
+    }
+  }
+
   beforeEach(() => {
-    document.body.innerHTML = `<div id="app"></div>`;
+    document.body.innerHTML = '<div id="app"></div>';
     router = new Router();
-    router.addRoute('/', PostsPage);
-    router.addRoute('/posts', PostsPage);
-    router.addRoute('/post', PostPage);
-    router.addRoute('*', NotFound);
+    router.addRoute('/', PostsPageMock);
+    router.addRoute('/posts', PostsPageMock);
+    router.addRoute('/post', PostPageMock);
+    router.addRoute('*', NotFoundPageMock);
   });
+
   describe('Posts Page', () => {
     it('/로 진입', async () => {
       await router.render('http://localhost');
